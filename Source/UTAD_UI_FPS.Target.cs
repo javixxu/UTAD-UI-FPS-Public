a3f427e3b71cc82f02728e74f8d06437c7226c81
XCJ Games <carlosjimenez@xcjgames.com>
2025-02-17 20:49:39 +0100

    Update to 5.4

--- Source/UTAD_UI_FPS.Target.cs
+++ Source/UTAD_UI_FPS.Target.cs
@@ -8,8 +8,8 @@ public class UTAD_UI_FPSTarget : TargetRules
 	public UTAD_UI_FPSTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("UTAD_UI_FPS");
 	}
 }
--- Source/UTAD_UI_FPSEditor.Target.cs
+++ Source/UTAD_UI_FPSEditor.Target.cs
@@ -8,8 +8,8 @@ public class UTAD_UI_FPSEditorTarget : TargetRules
 	public UTAD_UI_FPSEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("UTAD_UI_FPS");
 	}
 }
